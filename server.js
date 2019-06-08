d4c0dbd475163d99c2745bf7daeea742ea067b72
2016210402093 <806815937@qq.com>
2019-06-08 21:24:46 +0800

    v3

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let config = require("./config");
 let app = express();
+let server = require('http').Server(app)
+let io = require('socket.io')(server)
 
 const port = config.port;
 
@@ -34,6 +36,22 @@ const addTask = require('./middleware/addTask');
 const completeTask = require('./middleware/completeTask');
 const addClock = require('./middleware/addClock');
 const queryClockInfo = require('./middleware/queryClockInfo');
+const addEssay = require('./middleware/addEssay');
+const queryEssayInfo = require('./middleware/queryEssayInfo');
+const querySchoolInfo = require('./middleware/querySchoolInfo');
+const searchEssay = require('./middleware/searchEssay');
+const isLike = require('./middleware/isLike');
+const searchConcernedPerson = require('./middleware/searchConcernedPerson');
+const searchCollectEssay = require('./middleware/searchCollectEssay');
+const isConcernedUser = require('./middleware/isConcernedUser');
+const isCollectEssay = require('./middleware/isCollectEssay');
+const deleteEssay = require('./middleware/deleteEssay');
+const getEssayDetail = require('./middleware/getEssayDetail');
+const getOtherEssayDetail = require('./middleware/getOtherEssayDetail');
+const getConcernedNum = require('./middleware/getConcernedNum');
+const getFileInfo = require('./middleware/getFileInfo');
+const addComment = require('./middleware/addComment');
+const getCommentInfo = require('./middleware/getCommentInfo');
 
 
 app.use(express.static('public')); 
@@ -54,7 +72,32 @@ app.use('/addTask', addTask);
 app.use('/completeTask', completeTask);
 app.use('/addClock', addClock);
 app.use('/queryClockInfo', queryClockInfo);
+app.use('/addEssay', addEssay);
+app.use('/queryEssayInfo', queryEssayInfo);
+app.use('/querySchoolInfo', querySchoolInfo);
+app.use('/searchEssay', searchEssay);
+app.use('/isLike', isLike);
+app.use('/searchConcernedPerson', searchConcernedPerson);
+app.use('/searchCollectEssay', searchCollectEssay);
+app.use('/isConcernedUser', isConcernedUser);
+app.use('/isCollectEssay', isCollectEssay);
+app.use('/deleteEssay', deleteEssay);
+app.use('/getEssayDetail', getEssayDetail);
+app.use('/getOtherEssayDetail', getOtherEssayDetail);
+app.use('/getConcernedNum', getConcernedNum);
+app.use('/getFileInfo', getFileInfo);
+app.use('/addComment', addComment);
+app.use('/getCommentInfo', getCommentInfo);
 
+
+server.listen(5000);
+io.on('connection', (socket) => {
+  console.log('和客户端建立连接'+socket.id)
+  socket.on('message', (data) => {
+      console.log(data)
+      io.emit('server-message', data) // 服务器给客户端发送数据 在线聊天室
+  })
+})
 
 
 app.listen(config.port, function(){
