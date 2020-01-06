5cf4f212f3596c3d47a70339ab09f61b10cbd267
2016210402093 <43393082+2016210402093@users.noreply.github.com>
2020-01-06 21:11:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ let Config = {
         smtp: 'eukewzzhywldbfaa',
     },
     db: {
-        host: '47.102.199.210',
-        user: 'root',
-        password: '123456',
-        database: 'struggle'
+        host: '**********',
+        user: '****',
+        password: '******',
+        database: '******'
     }
 };
 module.exports = Config;
